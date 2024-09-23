d77b04ef5f65e96b625e3a546aa2f1e350acae12
ARJUN P <arjunpchr@gmail.com>
2024-09-23 17:41:06 +0530

    feat: started next project

---
+++ src/components/HeaderMain.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeaderMain = () => {
+  return (
+    <div className='border-b border-gray-200 py-6'>
+      HeaderMain
+    </div>
+  )
+}
+
+export default HeaderMain;
