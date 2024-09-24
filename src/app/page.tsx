7b124227b40c16b92a0db514c441bc3e2b47f1aa
ARJUN P <arjunpchr@gmail.com>
2024-09-24 16:09:53 +0530

    refactor: removed unwanted imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Hero from "@/components/Hero";
 import NewProduct from "@/components/NewProduct";
 import Testinomials from "@/components/Testinomials";
-import Image from "next/image";
 
 export default function Home() {
   return (
