dd3f49c41d49226bdf4bccd136bcd5f17f8f74e7
ARJUN P <arjunpchr@gmail.com>
2024-09-24 16:16:10 +0530

    refactor: footer reserve date modified to 2024

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="bg-blackish text-gray-500 text-center py-4 pb-16 md:pb-4">
       Copyright @ Logo <br />
-      All Rights Reserved 2023.
+      All Rights Reserved 2024.
     </div>
   );
 };
