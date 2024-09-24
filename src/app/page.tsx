8be6015370eb269316d6792627a4c664f1203f36
ARJUN P <arjunpchr@gmail.com>
2024-09-24 15:47:56 +0530

    feat: testinomial and mobile navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Hero from "@/components/Hero";
 import NewProduct from "@/components/NewProduct";
+import Testinomials from "@/components/Testinomials";
 import Image from "next/image";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <main>
       <Hero />
       <NewProduct />
+      <Testinomials />
     </main>
   );
 }
